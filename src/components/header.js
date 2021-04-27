7118e4de43d5ee19519709c446857e00d16ac459
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2021-04-27 16:55:46 -0300

    terminado

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,43 @@
-import * as React from "react"
-import PropTypes from "prop-types"
+import React from "react"
+import { css } from "@emotion/react"
+import styled from "@emotion/styled"
+import Navegacion from "./nav"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+const EnlaceHome = styled(Link)`
+  color: #fff;
+  text-align: center;
+  text-decoration: none;
+`
+
+const Header = () => {
+  return (
+    <header
+      css={css`
+        background-color: rgba(44,62,80);
+        padding: 1rem;
+      `}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+      <div
+        css={css`
+          max-width: 1200px;
+          margin: 0 auto;
+          @media (min-width: 768px) {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
+        `}
+      >
+        <EnlaceHome
+          to='/'
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+          <h1>Hotel Gatsby</h1>
+        </EnlaceHome>
+        <Navegacion />
+      </div>
+    </header>
+  )
 }
 
 export default Header
